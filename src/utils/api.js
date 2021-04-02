0c88f7f6722fd16c2c7ccd8927cdeef860f9042c
Dima Adamovich <60326258+DimaAdamovich@users.noreply.github.com>
2021-04-02 18:31:37 +0300

    add api key

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,16 @@
+import {API_KEY} from "../key";
+
 export const exchangeRequests = {
     getLatest: async (base) => {
         let data, error
         try{
-            let response = await fetch('https://api.exchangeratesapi.io/latest?base=' + base);
-            data = await response.json();
+            let response = await fetch(`https://api.exchangeratesapi.io/latest?access_key=${API_KEY}&base=` + base);
+            response = await response.json();
+            if(response.success){
+                data = response
+            }else {
+                error = response.error.info
+            }
         }catch (e) {
             error = e
         }
@@ -13,12 +20,12 @@ export const exchangeRequests = {
     getRate: async (base, symbol) => {
         let rate, error
         try{
-            let response = await fetch(`https://api.exchangeratesapi.io/latest?base=${base}&symbols=${symbol}`);
+            let response = await fetch(`https://api.exchangeratesapi.io/latest?access_key=${API_KEY}&base=${base}&symbols=${symbol}`);
             response =  await response.json();
-            if(response.rates){
+            if(response.success){
                 rate = response?.rates[symbol]
             }else {
-                error = response.error
+                error = response.error.info
             }
 
         }catch (e) {
